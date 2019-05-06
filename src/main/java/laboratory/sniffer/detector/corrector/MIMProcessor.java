5f572857c0d7dfe4c01390ef811893468ff176f1
Mouna <dm_boudjelida@esi.dz>
2019-05-06 15:53:30 +0100

    Resoudsre le probleme pour ajouter les variables, et extraire le traitement qui ecrit dans le file apres refactoring

--- src/main/java/laboratory/sniffer/detector/corrector/MIMProcessor.java
+++ src/main/java/laboratory/sniffer/detector/corrector/MIMProcessor.java
@@ -1,15 +1,13 @@
 package laboratory.sniffer.detector.corrector;
 
-import org.apache.log4j.Level;
 import spoon.processing.AbstractProcessor;
 import spoon.reflect.declaration.CtAnnotation;
-import spoon.reflect.declaration.CtClass;
+
 import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.declaration.ModifierKind;
 import utils.CsvReader;
+import utils.SaverOfTheFile;
 
-import java.io.*;
-import java.util.ArrayList;
 import java.util.HashSet;
 
 /**
@@ -27,7 +25,11 @@ public class MIMProcessor extends AbstractProcessor<CtMethod> {
         System.out.println("Processor MIMProcessor Start ... ");
         // Get applications information from the CSV - output
         meth_toStatic = CsvReader.formatCsv(file);
+
         //System.out.println("method to static "+meth_toStatic);
+
+
+
     }
 
     @Override
@@ -35,7 +37,9 @@ public class MIMProcessor extends AbstractProcessor<CtMethod> {
 
 
         Boolean chek=checkValidToCsv(candidate) && checkAnnotation(candidate);
+
         //System.out.println("check result "+chek);
+
         return checkValidToCsv(candidate) && checkAnnotation(candidate);
     }
 
@@ -43,49 +47,9 @@ public class MIMProcessor extends AbstractProcessor<CtMethod> {
 
         element.addModifier(ModifierKind.STATIC);
 
+        SaverOfTheFile fileSaver=new SaverOfTheFile();
+        fileSaver.reWriteFile(this,element);
 
-        try {
-            ArrayList<String> classFile = new ArrayList<>();
-
-
-            BufferedReader readFile = new BufferedReader(new FileReader(element.getPosition().getFile()));
-            String line = "";
-            while ((line = readFile.readLine()) != null) {
-
-                classFile.add(line);
-            }
-            getEnvironment().setAutoImports(true);
-
-            BufferedWriter writer = new BufferedWriter(new FileWriter(element.getPosition().getFile()));
-
-            CtClass theParentClass = null;
-            if (element.getParent() instanceof CtClass) {
-                theParentClass = (CtClass) element.getParent();
-
-            }
-            if (theParentClass != null) {
-
-                for (String s : classFile) {
-                    if(!s.matches(".*"+theParentClass.getSimpleName()+".*")) {
-                        writer.write(s);
-                        writer.newLine();
-                    }else {
-                        writer.write(element.getParent()+"");
-
-                        writer.newLine();
-                        writer.close();
-                        getEnvironment().report(this, Level.WARN, element, "INFO :" + element.getReference());
-                        return;
-
-                    }
-                }
-
-
-            }
-
-        }catch (IOException e) {
-            e.printStackTrace();
-        }
 
     }
 
@@ -95,9 +59,6 @@ public class MIMProcessor extends AbstractProcessor<CtMethod> {
 
         for(String occurence : meth_toStatic){
             String csvClassName = occurence.substring(occurence.lastIndexOf(".")+1);
-            // System.out.println("checkValidToCsv csv class name"+csvClassName);
-            //System.out.println("occurence .split "+occurence.split("#")[0]);
-            //System.out.println("condidate .get name .split "+candidate.getSimpleName().split("\\(")[0]);
 
             if(class_file.equals(csvClassName) &&
                     occurence.split("#")[0].equals(candidate.getSimpleName().split("\\(")[0])){
